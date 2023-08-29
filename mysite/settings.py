c88ffbc262c16f1cdfc3a2a56bedee7b81634b6b
LAPTOP-AJQEQR6H\001 <assddffggh0306@naver.com>
2023-08-29 18:34:27 +0900

    영식이 ㅋㅋㅋㅋㅋ

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+print(BASE_DIR)
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = BASE_DIR/'media'
